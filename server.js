0ed5041410f6411a024d4461664b1e438da21d8e
Heather Hern <heathernewlin1@gmail.com>
2020-12-04 14:02:23 -0500

    Added workout model

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ db.on("error", error => {
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "./public/index.html"));
+    res.sendFile(path.join(__dirname + "/Develop/public/index.html"));
 });
 
 app.post("/submit", (req, res) => {
